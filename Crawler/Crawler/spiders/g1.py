cd49c9be8859cb5894055860303f23f8f843dd3b
ghmc91 <gustavo_martins1991@hotmail.com>
2019-11-29 20:16:12 -0300

    Adicionando o subtítulo no Crawler

--- Crawler/Crawler/spiders/g1.py
+++ Crawler/Crawler/spiders/g1.py
@@ -10,17 +10,21 @@ class G1Spider(scrapy.Spider):
     start_urls = ['http://g1.globo.com/']
 
     def parse(self, response):
-        noticias = response.xpath('//a[@class="feed-post-link gui-color-primary gui-color-hover"]/text()').extract()
+        noticias = response.xpath('//div[@class="feed-post-body"]/div[@class="feed-post-body-title gui-color-primary gui-color-hover "]//a[@class="feed-post-link gui-color-primary gui-color-hover"]/text()').extract()
+        subtitulo = response.xpath('//div[@class="feed-post-body"]/div[@class="feed-post-body-resumo"]/div[@class="_label_event"]/text()').extract()
 
         noticias_dict = {
             'Página':{
             'url': response.url},
             'Notícia 1' : {
-            'Título': noticias[0]},
+            'Título': noticias[0],
+            'Subtítulo':subtitulo[0]},
             'Notícia 2' : {
-            'Título': noticias[1]},
+            'Título': noticias[1],
+            'Subtítulo':subtitulo[1]},
             'Notícia 3' : {
-            'Título': noticias[2]}
+            'Título': noticias[2],
+            'Subtítulo': subtitulo[2]}
             }
         
         with open('noticias.json', 'w', encoding='utf-8') as json_file:
